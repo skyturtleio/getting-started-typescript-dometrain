47403df23be87ce7b12cf81fb77252287c4c45af
Leo Mendoza <dev@skyturtle.io>
2025-09-09 23:19:04 -0700

    work through code

--- 4-typing-functions/4.1-typing-functions-basics/example4.ts
+++ 4-typing-functions/4.1-typing-functions-basics/example4.ts
@@ -1,9 +1,17 @@
+import { Effect } from "effect";
 // can also declare the type of a function variable:
 const multiply: (a: number, b: number) => number = function (a, b) {
   return a * b;
 };
 
 // Or, can use arrow syntax
-const divide: (a: number, b: number) => number = (a, b) => {
-  return a / b;
-};
+const divide = (a: number, b: number) =>
+  Effect.suspend(() => {
+    if (b === 0) {
+      return Effect.fail(new Error("Cannot divide by zero"));
+    }
+    return Effect.succeed(a / b);
+  });
+
+console.log(divide(4, 0));
+console.log(divide(20, 4));
--- 4-typing-functions/4.1-typing-functions-basics/exercise.ts
+++ 4-typing-functions/4.1-typing-functions-basics/exercise.ts
@@ -1,32 +1,38 @@
 // Exercise: Typing Functions in TypeScript
 
 // 1. Type this function to accept `num` of type `number` and returns a `boolean`.
-function isEven(num) {
+function isEven(num: number): boolean {
   return num % 2 === 0;
 }
 
 // 2. Type this function to accept two parameters of type `number` and return their product as a `number`.
-const multiply = (a, b) => {
+const multiply = (a: number, b: number): number => {
   return a * b;
 };
 
 // 3. Type this function to accept `message` of type `string`, and return `void`.
-function logError(message) {
+function logError(message: string): void {
   console.error(message);
 }
 
 // 4. Type this function to accept `arr` as an array that contains anything, and returns a `number`.
-function getLength(arr) {
+function getLength(arr: Array<any>): number {
   return arr.length;
 }
 
 // 5. Type this function to accept `name` of type `string` and `age` of type `number`, and return
 // an object with properties `name` and `age`.
-function createUser(name, age) {
+function createUser(
+  name: string,
+  age: number,
+): {
+  name: string;
+  age: number;
+} {
   return { name, age };
 }
 
 // 6. Type this function expression to accept `width` and `height` of type `number` and return a `number`.
-const calculateArea = function (width, height) {
+const calculateArea = function (width: number, height: number): number {
   return width * height;
 };
